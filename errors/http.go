5ade387527e04c4b8bd27dfd2a1e94b1c2c6a8de
Artha Dede <arthadede@gmail.com>
2024-09-10 15:39:10 +0800

    FEAT: add error not found

--- errors/http.go
+++ errors/http.go
@@ -11,11 +11,31 @@ type BadRequestError struct {
 	message string
 }
 
+func (e *BadRequestError) Error() string {
+	return e.message
+}
+
 func NewBadRequestError(err validator.FieldError) *BadRequestError {
 	return &BadRequestError{
 		field:   err.Field(),
 		tag:     err.Tag(),
 		value:   err.Value(),
 		message: err.Error(),
+	}
+}
+
+type NotFoundError struct {
+	entity  string
+	message string
+}
+
+func (e *NotFoundError) Error() string {
+	return e.message
+}
+
+func NewNotFoundError(entity string) *NotFoundError {
+	return &NotFoundError{
+		entity:  entity,
+		message: "data not found (" + entity + ")",
 	}
 }
