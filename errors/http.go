4cc4e644b1bd973873e44903f6d77cae2a856405
Artha Dede <arthadede@gmail.com>
2024-09-10 21:23:07 +0800

    FEAT: update custom errors

--- errors/http.go
+++ errors/http.go
@@ -1,55 +1,11 @@
 package errors
 
-import (
-	"github.com/go-playground/validator/v10"
-	"github.com/gofiber/fiber/v2"
-)
+import "errors"
 
-type CustomError interface {
-	Error() string
-	Status() int
-}
-
-type BadRequestError struct {
-	field   string
-	tag     string
-	value   interface{}
-	message string
-}
-
-func (e *BadRequestError) Error() string {
-	return e.message
-}
-
-func (e *BadRequestError) Status() int {
-	return fiber.StatusBadRequest
-}
-
-func NewBadRequestError(err validator.FieldError) *BadRequestError {
-	return &BadRequestError{
-		field:   err.Field(),
-		tag:     err.Tag(),
-		value:   err.Value(),
-		message: err.Error(),
-	}
-}
-
-type NotFoundError struct {
-	entity  string
-	message string
-}
-
-func (e *NotFoundError) Error() string {
-	return e.message
-}
-
-func (e *NotFoundError) Status() int {
-	return fiber.StatusNotFound
-}
-
-func NewNotFoundError(entity string) *NotFoundError {
-	return &NotFoundError{
-		entity:  entity,
-		message: "data not found (" + entity + ")",
-	}
-}
+var ErrNotFound = errors.New("error data not found")
+var ErrInsert = errors.New("error inserting data")
+var ErrUpdate = errors.New("error updating data")
+var ErrDelete = errors.New("error deleting data")
+var ErrValidateStruct = errors.New("error validating struct")
+var ErrParsingBody = errors.New("error parsing body")
+var ErrSomethingWentWrong = errors.New("error something went wrong")
