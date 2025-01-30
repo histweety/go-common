016dd2d540c78d499047fda2e5eb86d79499fa68
Artha Dede <arthadede@gmail.com>
2025-01-30 16:59:32 +0800

    FEAT: add seeding handler

---
+++ handlers/seeding.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type SeedingData struct {
+}
+
+func Seeding(db *mongo.Database, path string) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		file, err := os.Open(path)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+		}
+		defer file.Close()
+
+		var seeders map[string]interface{}
+		byteValue, _ := io.ReadAll(file)
+		json.Unmarshal(byteValue, &seeders)
+
+		for collectionName, collectionValues := range seeders {
+			collection := db.Collection(collectionName)
+			_, err := collection.InsertMany(c.Context(), collectionValues.([]interface{}))
+			if err != nil {
+				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+			}
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success"})
+	}
+}
