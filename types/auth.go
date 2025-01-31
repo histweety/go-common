e58b07a9b2a4474f403ee12104df208a27ec0e05
Artha Dede <arthadede@gmail.com>
2025-01-31 22:26:50 +0800

    FEAT: add auth config

--- types/auth.go
+++ types/auth.go
@@ -2,6 +2,11 @@ package types
 
 import "github.com/dgrijalva/jwt-go"
 
+type ConfigAuth struct {
+	Secret        string
+	RefreshSecret string
+}
+
 type Claims struct {
 	UserID string `json:"user_id"`
 	jwt.StandardClaims
